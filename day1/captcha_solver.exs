7544fb91d8173b5996eb4e07332408493012153f
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-12-01 21:50:21 +0530

    day 1 100 percent

--- day1/captcha_solver.exs
+++ day1/captcha_solver.exs
@@ -1,5 +1,5 @@
 defmodule Captcha do
-  def solve(input) do
+  def solve_next_digit(input) do
     digits = Integer.digits(input)
     digits = digits ++ [hd(digits)]
     reduce(digits, 0)
@@ -8,6 +8,18 @@ defmodule Captcha do
   defp reduce([], acc), do: acc
   defp reduce([a, a | rest], acc), do: reduce([a | rest], a + acc)
   defp reduce([_a | rest], acc), do: reduce(rest, acc)
+
+  def solve_halfway_next(input) do
+    digits = Integer.digits(input)
+    halfway_len = round(length(digits) / 2)
+    {first_half, second_half} = Enum.split(digits, halfway_len)
+    first = Enum.zip(first_half, second_half) |> Enum.map(&reduce_pairs/1) |> Enum.sum()
+    second = Enum.zip(second_half, first_half) |> Enum.map(&reduce_pairs/1) |> Enum.sum()
+    first + second
+  end
+
+  defp reduce_pairs({a, a}), do: a
+  defp reduce_pairs(_), do: 0
 end
 
 ExUnit.start()
@@ -17,12 +29,22 @@ defmodule CaptchaTest do
 
   import Captcha
 
-  test "solve captcha" do
-    assert solve(1122) == 3
-    assert solve(1111) == 4
-    assert solve(1234) == 0
-    assert solve(91212129) == 9
-    {input, ""} = File.read!("input.data") |> String.strip |> Integer.parse
-    assert solve(input) == 1182
+  @tag :skip
+  test "solve_next_digit captcha" do
+    assert solve_next_digit(1122) == 3
+    assert solve_next_digit(1111) == 4
+    assert solve_next_digit(1234) == 0
+    assert solve_next_digit(91_212_129) == 9
+    {input, ""} = File.read!("input.data") |> String.trim() |> Integer.parse()
+    assert solve_next_digit(input) == 1182
+  end
+
+  test "solve_halfway_next captcha" do
+    assert solve_halfway_next(1212) == 6
+    assert solve_halfway_next(123_425) == 4
+    assert solve_halfway_next(123_123) == 12
+    assert solve_halfway_next(12_131_415) == 4
+    {input, ""} = File.read!("input.data") |> String.trim() |> Integer.parse()
+    assert solve_halfway_next(input) == 1152
   end
 end
